9518064dd06beb6439e7eb76d11f5b01113fed42
Administrator <Administrator@PC-20180915JHTU>
2019-05-31 14:50:07 +0800

    第一次 提交

---
+++ src/main/java/com/oracle/git/Test.java
@@ -0,0 +1,12 @@
+package com.oracle.git;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("这是我的第一个git项目");
+
+	}
+
+}
